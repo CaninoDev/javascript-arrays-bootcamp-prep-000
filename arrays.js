9442cae1c604d893e9ac988dbd2b9dd7eaf7e428
Ian Canino <ian.canino@gmail.com>
2018-03-23 02:01:29 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = new Array();
+
+chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ]
+
+function addElementToBeginningOfArray(array, element) {
+  return ([element, ...array])
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return (array.unshift(element))
+}
+
+function accessElementInArray(array, index) {
+  return (array[index])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return (array.shift())
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return (array.slice(1))
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return (array.pop())
+}
+
